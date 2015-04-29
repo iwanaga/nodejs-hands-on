1ad16f70bc3d0d0b9d6e2ead4554788d5675d93c
iwanaga <iwanaga@iij.ad.jp>
2015-04-29 20:51:56 +0900

    ファイルの非同期読み込み

---
+++ 01_fs/async_file_read.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+var fileName = '../LICENSE';
+
+fs.readFile(fileName, function (error, data) {
+    if (error) {
+        throw error;
+    }
+
+    console.log('✓ Callback');
+    console.log(data.toString());
+});
+
+console.log('✓ Bottom line');
