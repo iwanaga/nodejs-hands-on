bab5d837a95f906dd85bc9c923f0087970e9f82d
iwanaga <iwanaga@iij.ad.jp>
2015-05-01 20:32:36 +0900

    generated by angular-fullstack

---
+++ 03_todo_app/client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('todoAppApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
